1a67d89256804efbbe60c4bff807098a896a53b0
Reeann <reeannvirginia@gmail.com>
2017-12-16 17:14:49 -0500

    adding organizations to funds show page

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,7 +1,7 @@
 class Fund < ApplicationRecord
   has_many :donations
   has_many :donors, through: :donations
-  has_many :organization_funds
+  has_many :organization_funds, dependent: :destroy
   has_many :organizations, through: :organization_funds
 
 end
