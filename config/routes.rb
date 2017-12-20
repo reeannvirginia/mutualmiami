9dd637bc96f9e8f2acbe6ec5e892b5c2c339eff1
Joy Clayton <lovejjoy@gmail.com>
2017-12-20 11:15:01 -0500

    updated route.rb folder

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :charges, only: [:new, :create]
   devise_for :donor_logins
   # devise_for :donors
-  resources :posts
+
   resources :funds
   resources :donations
   resources :organizations
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
 get 'thanks', to: 'charges#thanks', as: 'thanks'
 
- root to: 'posts#index'
+ root to: 'funds#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
