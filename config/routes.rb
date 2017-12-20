c619c1a55778e3fc33f86447ab4e20e059b0f061
Joy Clayton <lovejjoy@gmail.com>
2017-12-19 19:53:51 -0500

    template file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   resources :organizations
   resources :donors
 
+
+get 'thanks', to: 'charges#thanks', as: 'thanks'
+
  root to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
