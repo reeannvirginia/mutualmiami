a9c354f121729480eafd4494c5eefdc4188385b7
Reeann <reeannvirginia@gmail.com>
2017-12-28 14:12:26 -0500

    Update application

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'dotenv/load'
+if ["development", "test"].include?(Rails.env)
+  require 'dotenv/load'
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
